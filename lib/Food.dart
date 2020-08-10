ed925e111482b5a14742ae7a2d77b1041d9f1a88
Yazdani <yaz4dani@gmail.com>
2020-08-10 17:01:22 +0600

    Food Page

--- lib/Food.dart
+++ lib/Food.dart
@@ -33,10 +33,254 @@ class _FoodState extends State<Food> {
               ),
             ),
           ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+
+          Container(
+            height: 60.0,
+            margin: EdgeInsets.all(10.0),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Center(
+              child: Text("Apple",
+                style: TextStyle(fontSize: 20.0,color: Colors.white),
+              ),
+            ),
+          ),
+
           
         ],
       ),
-      
+
     );
   }
 }
